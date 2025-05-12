2e229379faa14b58f0840e199e54e6dc04df33ca
dcestaro5507 <dcestaro5507@floridapoly.edu>
2025-05-12 17:50:28 +0000

    Create first program

---
+++ week1/01_first_program/01_first_program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// using directory
+using namespace std;
+// ^^^ means I don't have to use "std::"
+using std::cout;
+
+   // cout - console output
+   // endl - new line
+   // << stream insertion operator
+   // :: scope resolution operator
+   // std - standard namespace
+
+int main() {
+    // Print a welcome message
+    cout << "Hello COP3337" << endl << endl;
+    cout << "5 / 2 is " << 5 / 2 << endl << endl;
+
+    cout << "7 + (3 * 5) = " << 7 + (3 * 5) << endl;
+  
+
+
+
+return 0;
+}
