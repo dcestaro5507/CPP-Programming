a178c6a890699f93f00e089402b44819ff7a8f7b
dcestaro5507 <dcestaro5507@floridapoly.edu>
2025-05-15 16:53:08 +0000

    Quiz 1 Study

---
+++ week1/04_quiz1_study.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+
+    string str = "OOP";
+    for (char c : str){
+        cout << c << endl;
+    }
+
+
+
+    return 0;
+}
